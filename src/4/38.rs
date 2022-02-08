b7425b4d929d0e3feb8660e293110a9acf376b51
taemath <hirotaka081630@gmail.com>
2022-02-08 20:37:17 +0900

    feat(39): やっつけな Option/Result 処理

--- src/4/38.rs
+++ src/4/38.rs
@@ -5,12 +5,9 @@ fn do_something_that_might_fail(i: i32) -> Result<f32, String> {
     Err(String::from("正しい値ではありません"))
   }
 }
+
 fn main() -> Result<(), String> {
-  let v = do_something_that_might_fail(42).unwrap();
+  let v = do_something_that_might_fail(421)?;
   println!("発見 {}", v);
-
-  let v = do_something_that_might_fail(1).unwrap();
-  println!("発見 {}", v);
-
   Ok(())
 }
