7e35cb15082c000b83a26c9547e4c2dad28ab74c
taemath <hirotaka081630@gmail.com>
2022-02-08 18:52:40 +0900

    feat(35): option(ジェネリックな列挙型)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+// 部分的に定義された構造体型
 struct BagOfHolding<T> {
-  item: T,
+  item: Option<T>,
 }
-fn main() {
-  // 注意: ジェネリック型を使用すると、型はコンパイル時に作成される。
-  // ::<> (turbofish) で明示的に型を指定
-  let i32_bag = BagOfHolding::<i32> { item: 42 };
-  let bool_bag = BagOfHolding::<bool> { item: true };
-  // ジェネリック型でも型推論可能
-  let float_bag = BagOfHolding { item: 3.14 };
-  // 注意: 実生活では手提げ袋を手提げ袋に入れないように
-  let bag_in_bag = BagOfHolding {
-    item: BagOfHolding { item: "boom!" },
-  };
+// パラメータ T を渡すことが可能
 
-  println!(
-    "{} {} {} {}",
-    i32_bag.item, bool_bag.item, float_bag.item, bag_in_bag.item.item
-  );
+fn main() {
+  // 注意: i32 が入るバッグに、何も入っていません！
+  // None からは型が決められないため、型を指定する必要があります。
+  let i32_bag = BagOfHolding::<i32> { item: None };
+
+  if i32_bag.item.is_none() {
+    println!("バッグには何もない！")
+  } else {
+    println!("バッグには何かある！")
+  }
+
+  let i32_bag = BagOfHolding::<i32> { item: Some(42) };
+
+  if i32_bag.item.is_some() {
+    println!("バッグには何かある！")
+  } else {
+    println!("バッグには何もない！")
+  }
+  // match は Option をエレガントに分解して、
+  // すべてのケースが処理されることを保証できます！
+  match i32_bag.item {
+    Some(v) => println!("バッグに {} を発見！", v),
+    None => println!("何も見つからなかった"),
+  }
 }
