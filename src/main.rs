3f64843218a156817e3fd90035f109fadf29f9b2
taemath <hirotaka081630@gmail.com>
2022-02-08 20:04:55 +0900

    feat(37): 失敗するかもしれないmain

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
-fn do_something_that_fail(i: i32) -> Result<f32, String> {
+fn do_something_that_might_fail(i: i32) -> Result<f32, String> {
   if i == 42 {
     Ok(13.0)
   } else {
     Err(String::from("正しい値ではありません"))
   }
 }
-
-fn main() {
-  let result = do_something_that_fail(12);
+fn main() -> Result<(), String> {
+  let result = do_something_that_might_fail(12);
 
   match result {
     Ok(v) => println!("発見 {}", v),
-    Err(e) => println!("Error: {}", e),
+    Err(_e) => {
+      return Err(String::from("main で何か問題が起きました"));
+    }
   }
-
-  let result = do_something_that_fail(42);
-  match result {
-    Ok(v) => println!("発見 {}", v),
-    Err(e) => println!("Error: {}", e),
-  }
+  Ok(())
 }
