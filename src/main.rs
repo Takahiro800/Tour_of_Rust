718c09f7ab241cbde93daa0c6ff9c3856a980144
taemath <hirotaka081630@gmail.com>
2022-02-09 20:08:03 +0900

    feat(48): 参照による所有権の借用

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 struct Foo {
   x: i32,
 }
-
-fn do_something() -> Foo {
-  Foo { x: 42 }
-  // 所有権は外に移動
-}
-
 fn main() {
-  let foo = do_something();
-  // foo は所有者になる
-
-  println!("{}", foo.x);
-  // 関数のスコープの終端により、foo はドロップ
+  let foo = Foo { x: 42 };
+  let f = &foo;
+  println!("{}", f.x);
+  println!("{:p}", f);
+  println!("{}", f.x);
 }
