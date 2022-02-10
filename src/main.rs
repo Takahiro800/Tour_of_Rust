b7e57dc533a8908261cb4035979387599db63eeb
taemath <hirotaka081630@gmail.com>
2022-02-10 12:36:13 +0900

    feat(59): 字列リテラル

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-struct Foo<'a> {
-  i: &'a i32,
-}
-
-struct Bar {
-  i: i32,
-}
 fn main() {
-  let x = 42;
-  let foo = Foo { i: &x };
-  println!("{}", foo.i);
-
-  let y = 13;
-  let bar = Bar { i: y };
-  println!("{}", bar.i);
+  let a: &'static str = "こんにちは 🦀";
+  println!("{} {}", a, a.len());
 }
